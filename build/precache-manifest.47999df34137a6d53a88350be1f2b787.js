8f3cbdf2ece391f12d196a53c26e2a6519a21757
Bat-Sheva-Sh <shevi4216@gmail.com>
2023-05-01 02:05:43 +0300

    first commit

---
+++ build/precache-manifest.47999df34137a6d53a88350be1f2b787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25d5b145ff025f1de764134deb177030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55167ff0d162aa1d4480",
+    "url": "/static/js/2.c39003cf.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/static/js/2.c39003cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a6e95ad74f551bc4353",
+    "url": "/static/js/main.2e5f6527.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.2e5f6527.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed24628c41b89e511d36",
+    "url": "/static/js/runtime-main.76e1faf0.js"
+  }
+]);
